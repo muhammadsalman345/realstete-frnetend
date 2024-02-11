4a0604b667f5893ce58cff1509adf250ca2db3ab
muhammadsalman345 <salmankhanhissam345@gmail.com>
2024-02-11 06:31:06 -0800

    Your commit message

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  api: {
+    server: 'https://adorable-cod-polo-shirt.cyclic.app/',
+    mapKey: '',
+    googleAuthClientId: '',
+    webSocketUrl: ''
+  }
+};
